145ba61a6d584da2d2170f610051d7c171d69e68
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-06-11 23:52:49 +0530

    Create palindrome.java

---
+++ Patterns/palindrome.java
@@ -0,0 +1,18 @@
+
+
+public class palindrome {
+    public static boolean palindromeString(String ch) {
+        if (ch == null || ch.length() <= 1) {
+            return true; // Empty string or single character string is a palindrome
+        }
+
+        StringBuilder sb = new StringBuilder(ch);
+        sb.reverse();
+        return sb.toString().equals(ch);
+    }
+
+    public static void main(String[] args) {
+        String a = "010";
+        System.out.println(palindromeString(a));
+    }
+}
