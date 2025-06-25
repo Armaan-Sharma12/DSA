ce60082ea4b514baf4752b90f42f9d9117a9cfde
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-06-25 20:40:02 +0530

    Create RemoveDuplicates.java

---
+++ LinkedList/RemoveDuplicates.java
@@ -0,0 +1,34 @@
+package com.LinkedList;
+
+public class RemoveDuplicates {
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        while (node != null && node.next != null) {
+            if(node.val == node.next.val) {
+                node.next = node.next.next;
+            }
+            else{
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
