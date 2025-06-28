8ce5d89cfe316e8848028bb5b83506db77d8bde7
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-06-28 16:16:23 +0530

    Create CustomQueue.java

---
+++ Stacks and Queues/CustomQueue.java
@@ -0,0 +1,35 @@
+package com.StacksAndQueues;
+
+import java.util.ArrayList;
+
+public class CustomQueue<T>  {
+    private ArrayList<T> queue;
+
+    public CustomQueue() {
+        this.queue = new ArrayList<>();
+    }
+
+    public void display() {
+        System.out.println("Stack :" + queue.toString());
+    }
+
+    public void enqueue(T item) {
+        queue.add(item);
+    }
+
+    public T dequeue() {
+        return queue.remove(0);
+    }
+
+    public T peek() {
+        return queue.get(0);
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+}
