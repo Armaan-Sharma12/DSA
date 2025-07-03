c861e2ab9852808b8c666efc99e2c76dc62de6a1
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-04 00:54:01 +0530

    Create Stringbuf.java

---
+++ Strings/Stringbuf.java
@@ -0,0 +1,11 @@
+package com.StringBuffer;
+
+public class Stringbuf {
+    public static void main(String[] args) {
+//        StringBuffer sb = new StringBuffer("Hello");
+//        sb.append(" World");
+//        System.out.println(sb.toString());
+
+        GenerateRandomString.generate(10);
+    }
+}
