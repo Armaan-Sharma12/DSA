45d4fb64826511006c37ed063a2c212b2d0d32d6
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-09 19:55:42 +0530

    Update RotateArray.java

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -1,27 +1,25 @@
 
+
 public class RotateArray {
     public static void main(String[] args) {
 
     }
-    public static void rotateArr(int[] arr, int d) {
-        // add your code here
-        if (arr == null || arr.length == 0 || d < 0) {
-            return;
+        public void rotate(int[] arr, int d) {
+            int n = arr.length;
+            if (arr == null || n == 0 || d < 0) return;
+            int steps = d % (n);
+            if (steps == 0) return;
+            int[] ans = new int[n];
+
+            for (int i = 0; i < steps; i++) {
+                ans[i] = arr[n - d + i];
+            }
+            for (int i = 0; i < n - steps; i++) {
+                ans[d + i] = arr[i];
+            }
+            for (int i = 0; i < n; i++) {
+                arr[i] = ans[i];
+            }
         }
-        int[] ans = new int[arr.length];
-        int steps = d % (arr.length);
-        int end = arr.length-1;
-        int p =arr.length -1;
-        for(int i = steps-1;i>=0;i-- ){
-            ans[p] = arr[i];
-            p--;
-        }
-        for(int i = 0; i<arr.length-steps;i++){
-            ans[i] = arr[steps];
-            steps++;
-        }
-        for(int i = 0 ; i<=end;i++){
-            arr[i] = ans[i];
-        }
-    }
+
 }
