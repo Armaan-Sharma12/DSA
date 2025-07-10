27309200699cd42dcb78e69cc81998ff7f68df4f
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-10 17:04:43 +0530

    Create LevelOrder2.java

---
+++ Trees/LevelOrder2.java
@@ -0,0 +1,52 @@
+package com.Tree;
+
+import java.util.*;
+//https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+public class LevelOrder2 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+
+            levels.add(level);
+        }
+        Collections.reverse(levels);
+        return levels;
+    }
+}
