6d37742f9959294e6217cb156353084f4dcaeefe
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-14 22:36:31 +0530

    Create SortedArrayToBST.java

---
+++ Trees/SortedArrayToBST.java
@@ -0,0 +1,32 @@
+package com.Tree;
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+public class SortedArrayToBST {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) return null;
+        return insert(nums,0, nums.length-1 );
+    }
+    public TreeNode insert(int[] arr, int start, int end){
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end-start)/2;
+        TreeNode node = new TreeNode(arr[mid]);
+        node.left = insert(arr,start,mid-1);
+        node.right = insert(arr,mid+1,end);
+        return node;
+    }
+
+}
