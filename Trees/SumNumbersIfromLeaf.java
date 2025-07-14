275750aa9faeca8a632acba1127407c0b006db63
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-14 22:37:36 +0530

    Create SumNumbersIfromLeaf.java

---
+++ Trees/SumNumbersIfromLeaf.java
@@ -0,0 +1,25 @@
+package com.Tree;
+//https://leetcode.com/problems/sum-root-to-leaf-numbers/
+public class SumNumbersIfromLeaf {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int sumNumbers(TreeNode root){
+         return helper(root,0);
+    }
+    private int helper(TreeNode root, int sum){
+        if(root==null) return 0;
+        sum = sum*10+root.val;
+        if(root.left==null && root.right==null) return sum;
+        return helper(root.left,sum) + helper(root.right,sum);
+    }
+}
