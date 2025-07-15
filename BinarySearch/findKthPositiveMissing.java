bd3f3faa376599735bf219c3ac58fd8ccc1ccd79
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-15 16:36:23 +0530

    Create findKthPositiveMissing.java

---
+++ BinarySearch/findKthPositiveMissing.java
@@ -0,0 +1,25 @@
+package com.binarySearch;
+//https://leetcode.com/problems/kth-missing-positive-number/submissions/1693114122/
+import java.util.ArrayList;
+
+public class findKthPositiveMissing {
+    public int findKthPositive(int[] arr, int k) {
+//        ArrayList<Integer> list = new ArrayList<>();
+        int start = 0;
+        int i = 1;
+        int miss = 0;
+        while(miss< k){
+            if(start<arr.length && arr[start]==i){
+                start++;
+            }
+            else{
+                miss++;
+                if(miss==k){
+                    return i;
+                }
+            }
+            i++;
+        }
+        return -1;
+    }
+}
