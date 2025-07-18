098949a3fb21a7a46c61e0d3ebda69c91ffc417f
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-18 22:08:11 +0530

    Create Main.java

---
+++ Heap/Main.java
@@ -0,0 +1,24 @@
+package com.Heap;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Heap<Integer> heap = new Heap<>();
+        heap.insert(10);
+        heap.insert(20);
+        heap.insert(30);
+        heap.insert(40);
+        heap.insert(50);
+        heap.insert(60);
+        heap.insert(70);
+        heap.insert(80);
+        heap.insert(90);
+        heap.insert(3);
+//        System.out.println(heap.remove());
+//        System.out.println(heap.remove());
+
+        ArrayList<Integer> hwat = heap.heapsort();
+        System.out.println(hwat);
+    }
+}
