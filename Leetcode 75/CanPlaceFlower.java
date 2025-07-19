2072779202165e01a06954071496fc5eaf1e0b27
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-20 02:07:13 +0530

    Create CanPlaceFlower.java

---
+++ Leetcode 75/CanPlaceFlower.java
@@ -0,0 +1,17 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+public class CanPlaceFlower {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed.length==0) return false;
+        if(n==0) return true;
+        int count = 0 ;
+        for(int i = 0 ; i < flowerbed.length ; i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            if(count==n) return true;
+        }
+        return false;
+    }
+}
