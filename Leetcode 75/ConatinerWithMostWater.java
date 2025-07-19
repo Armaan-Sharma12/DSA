f168ca1786d2f423eb81c7c8a313763f52de4eb1
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-20 02:08:07 +0530

    Create ConatinerWithMostWater.java

---
+++ Leetcode 75/ConatinerWithMostWater.java
@@ -0,0 +1,19 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/container-with-most-water/submissions/1703299003/?envType=study-plan-v2&envId=leetcode-75
+public class ConatinerWithMostWater {
+    class Solution {
+        public int maxArea(int[] height) {
+            int n = height.length;
+            int left = 0;
+            int right = n-1;
+            int maxw = Integer.MIN_VALUE;
+
+            while(left<right){
+                maxw = Math.max(maxw,Math.min(height[left],height[right])*(right-left));
+                if(height[left]<height[right]) left++;
+                else right--;
+            }
+            return maxw;
+        }
+    }
+}
