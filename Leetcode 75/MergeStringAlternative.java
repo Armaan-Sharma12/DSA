ae7f0546cd06de983e0cbb0d117d4f52cbc7f02d
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-20 02:10:47 +0530

    Create MergeStringAlternative.java

---
+++ Leetcode 75/MergeStringAlternative.java
@@ -0,0 +1,19 @@
+
+//https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
+public class MergeStringAlternative {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int maxLength = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < word1.length()) {
+                merged.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                merged.append(word2.charAt(i));
+            }
+        }
+
+        return merged.toString();
+    }
+}
