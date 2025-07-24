2054f8afb8eb29084be4729d32238f1195d4ad98
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-24 19:53:51 +0530

    Create FindHighestAltitude.java

---
+++ Leetcode 75/FindHighestAltitude.java
@@ -0,0 +1,18 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/find-the-highest-altitude?envType=study-plan-v2&envId=leetcode-75
+public class FindHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int[] result = new int[gain.length+1];
+        result[0]=0;
+
+        for(int i =1; i<=gain.length;i++){
+            result[i] = gain[i-1]+result[i-1];
+        }
+
+        int maxElement=0;
+        for(int max : result){
+            maxElement = Math.max(maxElement,max);
+        }
+        return maxElement;
+    }
+}
