af0409ae044cb5389933c76fd9f5200e2e01fc75
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-24 19:55:23 +0530

    Create LongestSubArrayOfOneAfterDeleting.java

---
+++ Leetcode 75/LongestSubArrayOfOneAfterDeleting.java
@@ -0,0 +1,34 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=study-plan-v2&envId=leetcode-75
+public class LongestSubArrayOfOneAfterDeleting {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxLength = 0;
+
+        boolean hasZero = false;
+        for(int num : nums){
+            if(num == 0){
+                hasZero = true;
+                break;
+            }
+        }
+        if (!hasZero) {
+            return nums.length -1;
+        }
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            while(zeroCount>1){
+                if(nums[left]==0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength,right-left);
+        }
+        return maxLength;
+    }
+}
