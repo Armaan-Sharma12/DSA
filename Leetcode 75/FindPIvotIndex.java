ae5f3e4801ff3a537df7ff1ccdd8be3101789528
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-24 19:54:29 +0530

    Create FindPIvotIndex.java

---
+++ Leetcode 75/FindPIvotIndex.java
@@ -0,0 +1,19 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/find-pivot-index/?envType=study-plan-v2&envId=leetcode-75
+public class FindPIvotIndex {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }
+        for(int i=0;i<nums.length;i++){
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+}
