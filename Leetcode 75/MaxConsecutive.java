d84a49ebd14bebd5a67bae8ea35a92b75dbcc5a9
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-24 19:56:02 +0530

    Create MaxConsecutive.java

---
+++ Leetcode 75/MaxConsecutive.java
@@ -0,0 +1,37 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/max-consecutive-ones/
+public class MaxConsecutive {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int current = 0;
+        for(int i = 0 ; i< nums.length ; i++){
+            if(nums[i]==1){
+                current++;
+            } else{
+                count = Math.max(count,current);
+                current = 0;
+            }
+        }
+        count = Math.max(count,current);
+        return count;
+    }
+
+    //https://leetcode.com/problems/max-consecutive-ones-iii/?envType=study-plan-v2&envId=leetcode-75
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                k--;
+            }
+
+            if (k < 0) {
+                if (nums[left] == 0) {
+                    k++;
+                }
+                left++;
+            }
+        }
+        return nums.length - left;
+    }
+}
