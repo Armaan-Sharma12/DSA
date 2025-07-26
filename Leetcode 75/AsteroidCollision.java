21acbaa7c2fdf3e40cfe63c51f4e0117141f8f25
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-26 19:03:27 +0530

    Create AsteroidCollision.java

---
+++ Leetcode 75/AsteroidCollision.java
@@ -0,0 +1,40 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/asteroid-collision/?envType=study-plan-v2&envId=leetcode-75
+import java.util.Stack;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        if(asteroids.length<=1) return asteroids;
+        Stack<Integer> stack = new Stack<>();
+        for(int num : asteroids){
+            if(num<0){
+                while(!stack.isEmpty() && stack.peek()>0){
+                    if(stack.peek()<Math.abs(num)){
+                        stack.pop();
+                    }
+                    else if(stack.peek()==Math.abs(num)){
+                        stack.pop();
+                        num = 0;
+                        break;
+                    }
+                    else{
+                        num = 0;
+                        break;
+                    }
+                }
+                if(num != 0 && (stack.isEmpty() || stack.peek()<0)){
+                    stack.push(num);
+                }
+            }
+            else{
+                stack.push(num);
+            }
+        }
+        int []  result = new int[stack.size()];
+        int index = 0 ;
+        for(int num:stack){
+            result[index++] = num;
+        }
+        return result;
+    }
+}
