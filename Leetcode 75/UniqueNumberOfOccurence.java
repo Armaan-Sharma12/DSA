065743c1cdade063e540a5d57e672418a52fdd8b
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-26 19:07:38 +0530

    Create UniqueNumberOfOccurence.java

---
+++ Leetcode 75/UniqueNumberOfOccurence.java
@@ -0,0 +1,13 @@
+package com.Leetcode75;
+
+import java.util.HashMap;
+//https://leetcode.com/problems/unique-number-of-occurrences/?envType=study-plan-v2&envId=leetcode-75
+public class UniqueNumberOfOccurence {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        return map.size()==map.values().stream().distinct().count();
+    }
+}
