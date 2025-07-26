7a72ba116de0755757e7bf5ac9781890fe40f82b
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-07-26 19:08:09 +0530

    Create RemovingStarFromSttring.java

---
+++ Leetcode 75/RemovingStarFromSttring.java
@@ -0,0 +1,24 @@
+package com.Leetcode75;
+//https://leetcode.com/problems/removing-stars-from-a-string/?envType=study-plan-v2&envId=leetcode-75
+import java.util.Stack;
+
+public class RemovingStarFromSttring {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='*'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else{
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
