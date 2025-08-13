fd391aa8ed6fd000b0ea87866a59a9a7fea5a0f3
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-08-13 09:58:54 +0530

    Create CountNode.java

---
+++ Trees/CountNode.java
@@ -0,0 +1,26 @@
+package com.Tree;
+
+public class CountNode {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    int count = 0 ;
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        count = helper(root);
+        return count;
+    }
+    public int helper(TreeNode root){
+        if(root == null) return 0;
+        return helper(root.left)+helper(root.right)+1;
+    }
+}
