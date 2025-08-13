c51f7a2f7680fe3ffcf99adcd65398ba58b795a8
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-08-13 09:55:34 +0530

    Create MergeKSortedLL.java

---
+++ LinkedList/MergeKSortedLL.java
@@ -0,0 +1,33 @@
+package com.LinkedList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MergeKSortedLL {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> list = new ArrayList<>();
+        for(ListNode node:lists){
+            while(node!=null){
+                list.add(node.val);
+                node = node.next;
+            }
+        }
+        Collections.sort(list);
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        for(int i:list){
+            curr.next = new ListNode(i);
+            curr = curr.next;
+        }
+        return head.next;
+    }
+
+}
