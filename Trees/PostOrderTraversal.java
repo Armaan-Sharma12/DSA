d254e6f62922205bbf45a8c625fe0eaf17c89ab8
Armaan Sharma <armaan.sharma2003@gmail.com>
2025-08-13 10:02:44 +0530

    Create PostOrderTraversal.java

---
+++ Trees/PostOrderTraversal.java
@@ -0,0 +1,30 @@
+package com.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostOrderTraversal {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root,list);
+        return list;
+    }
+    private void helper(TreeNode node, List<Integer> list){
+        if(node == null) return;
+        helper(node.left,list);
+        helper(node.right,list);
+        list.add(node.val);
+    }
+}
